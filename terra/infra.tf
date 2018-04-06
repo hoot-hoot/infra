349729ede1f8edfd06995e9ed105c542d3f8afb7
Horia Coman <horia141@gmail.com>
2018-04-06 18:14:35 +0300

    The live setup

--- terra/infra.tf
+++ terra/infra.tf
@@ -3,9 +3,14 @@ variable "ci_terraformer_creds" {}
 variable "container_registry_bucket" {}
 variable "staging_region" {}
 variable "staging_region_and_zone" {}
-variable "staging_sqldb_primary_user_identity_password" {}
-variable "staging_sqldb_primary_user_content_password" {}
+variable "staging_sqldb_main_user_identity_password" {}
+variable "staging_sqldb_main_user_content_password" {}
 variable "staging_compute_web_cluster_user_password" {}
+variable "live_region" {}
+variable "live_region_and_zone" {}
+variable "live_sqldb_main_user_identity_password" {}
+variable "live_sqldb_main_user_content_password" {}
+variable "live_compute_web_cluster_user_password" {}
 
 terraform {
   backend "gcs" {
@@ -18,7 +23,6 @@ terraform {
 
 provider "google" {
   credentials = "${file(var.ci_terraformer_creds)}"
-  project = "chmsqrt2-truesparrow-staging"
 }
 
 # # # # #
@@ -51,6 +55,15 @@ data "google_dns_managed_zone" "chmsqrt2-domain" {
 # # # # # #
 # COMMON  #
 # # # # # #
+
+resource "google_storage_bucket_iam_binding" "container-registry-storage-object-viewers" {
+  bucket = "${var.container_registry_bucket}"
+  role = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${google_service_account.staging-compute-web.email}",
+    "serviceAccount:${google_service_account.live-compute-web.email}"
+  ]
+}
 
 # # # # # # # # # # #
 # LOCAL ENVIRONMENT #
@@ -164,14 +177,6 @@ resource "google_project_iam_binding" "staging-cloudsql-clients" {
   ]
 }
 
-resource "google_storage_bucket_iam_binding" "staging-storage-object-viewers" {
-  bucket = "${var.container_registry_bucket}"
-  role = "roles/storage.objectViewer"
-  members = [
-    "serviceAccount:${google_service_account.staging-compute-web.email}"
-  ]
-}
-
 resource "google_compute_network" "staging-network" {
   project = "${google_project.staging.id}"
 
@@ -224,10 +229,10 @@ resource "google_compute_firewall" "staging-network-firewall" {
   }
 }
 
-resource "google_sql_database_instance" "staging-sqldb-primary" {
+resource "google_sql_database_instance" "staging-sqldb-main" {
   project = "${google_project.staging.id}"
 
-  name = "chmsqrt2-truesparrow-staging-sqldb-primary"
+  name = "chmsqrt2-truesparrow-staging-sqldb-main"
   database_version = "POSTGRES_9_6"
   region = "${var.staging_region}"
 
@@ -264,23 +269,23 @@ resource "google_sql_database_instance" "staging-sqldb-primary" {
 
 resource "google_sql_user" "staging-sqldb-user-identity" {
   project = "${google_project.staging.id}"
-  instance = "${google_sql_database_instance.staging-sqldb-primary.name}"
+  instance = "${google_sql_database_instance.staging-sqldb-main.name}"
 
   name = "service-identity"
-  password = "${var.staging_sqldb_primary_user_identity_password}"
+  password = "${var.staging_sqldb_main_user_identity_password}"
 }
 
 resource "google_sql_user" "staging-sqldb-user-content" {
   project = "${google_project.staging.id}"
-  instance = "${google_sql_database_instance.staging-sqldb-primary.name}"
+  instance = "${google_sql_database_instance.staging-sqldb-main.name}"
 
   name = "service-content"
-  password = "${var.staging_sqldb_primary_user_content_password}"
+  password = "${var.staging_sqldb_main_user_content_password}"
 }
 
 resource "google_sql_database" "staging-sqldb-database" {
-  # project = "${google_project.staging.id}"
-  instance = "${google_sql_database_instance.staging-sqldb-primary.name}"
+  project = "${google_project.staging.id}"
+  instance = "${google_sql_database_instance.staging-sqldb-main.name}"
 
   name = "truesparrow"
   charset = "UTF8"
@@ -430,41 +435,351 @@ resource "google_dns_record_set" "staging-sitefe-letsencrypt-challange" {
   rrdatas = [ "9uC_pYUkVe27ldkOqRSSmLYQVyj2o7JGl7upnnDyltQ", "UmxjwgMiLLSF0WbgpugcDUJ-xVqB5dwEhvcvZTQp7tw" ]
 }
 
-resource "google_compute_ssl_certificate" "staging-loadbalancer-new-certificate" {
-  project = "${google_project.staging.id}"
-
-  name = "chmsqrt2-truesparrow-staging-loadbalancer-new-certificate"
-  description = "Certificate for the staging global loadbalancer"
-
-  private_key = "${file("../certs-temp/privkey.pem")}"
-  certificate = "${file("../certs-temp/fullchain.pem")}"
-}
-
 resource "google_compute_ssl_certificate" "staging-loadbalancer-newnew-certificate" {
   project = "${google_project.staging.id}"
 
   name = "chmsqrt2-truesparrow-staging-loadbalancer-newnew-certificate"
   description = "Certificate for the staging global loadbalancer"
 
-  private_key = "${file("../certs-temp-new/privkey.pem")}"
-  certificate = "${file("../certs-temp-new/fullchain.pem")}"
+  private_key = "${file("../certs-staging/privkey.pem")}"
+  certificate = "${file("../certs-staging/fullchain.pem")}"
 }
 
 # # # # # # # # # # #
-# PROD ENVIRONMENT  #
+# LIVE ENVIRONMENT  #
 # # # # # # # # # # #
 
-resource "google_project" "prod" {
-  name = "Env - Prod"
-  project_id = "chmsqrt2-truesparrow-prod"
+resource "google_project" "live" {
+  name = "Env - Live"
+  project_id = "chmsqrt2-truesparrow-live"
   folder_id = "${data.google_active_folder.truesparrow.id}"
   billing_account = "${data.google_billing_account.truesparrow.id}"
 }
 
-resource "google_project_services" "prod-services" {
-  project = "${google_project.prod.id}"
+resource "google_project_services" "live-services" {
+  project = "${google_project.live.id}"
 
   services = [
-    "maps-embed-backend.googleapis.com"
+    "compute.googleapis.com",
+    "dns.googleapis.com",
+    "maps-embed-backend.googleapis.com",
+    "maps-backend.googleapis.com",
+    "places-backend.googleapis.com",
+    "sqladmin.googleapis.com",
+    "sql-component.googleapis.com",
+    "containerregistry.googleapis.com",
+    "pubsub.googleapis.com",
+    "deploymentmanager.googleapis.com",
+    "replicapool.googleapis.com",
+    "replicapoolupdater.googleapis.com",
+    "resourceviews.googleapis.com",
+    "container.googleapis.com",
+    "storage-api.googleapis.com"
   ]
+}
+
+resource "google_service_account" "live-compute-web" {
+  account_id = "compute-web"
+  display_name = "Compute Web"
+  project = "${google_project.live.id}"
+}
+
+resource "google_service_account" "live-service-identity" {
+  account_id = "service-identity"
+  display_name = "Identity Service"
+  project = "${google_project.live.id}"
+}
+
+resource "google_service_account" "live-service-content" {
+  account_id = "service-content"
+  display_name = "Content Service"
+  project = "${google_project.live.id}"
+}
+
+resource "google_service_account" "live-service-adminfe" {
+  account_id = "service-adminfe"
+  display_name = "Adminfe Service"
+  project = "${google_project.live.id}"
+}
+
+resource "google_service_account" "live-service-sitefe" {
+  account_id = "service-sitefe"
+  display_name = "Sitefe Service"
+  project = "${google_project.live.id}"
+}
+
+resource "google_project_iam_binding" "live-cloudsql-clients" {
+  project = "${google_project.live.id}"
+  role = "roles/cloudsql.client"
+  members = [
+    "serviceAccount:${google_service_account.live-service-identity.email}",
+    "serviceAccount:${google_service_account.live-service-content.email}"
+  ]
+}
+
+resource "google_compute_network" "live-network" {
+  project = "${google_project.live.id}"
+
+  name = "chmsqrt2-truesparrow-live-network"
+  description = "Common network"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+
+  depends_on = [ "google_project_services.live-services" ]
+}
+
+resource "google_compute_subnetwork" "live-subnetwork" {
+  project = "${google_project.live.id}"
+  network = "${google_compute_network.live-network.name}"
+
+  name = "chmsqrt2-truesparrow-live-subnetwork"
+  description = "Subnetwork in the default live zone"
+  region = "${var.live_region}"
+  ip_cidr_range = "10.10.0.0/16"
+  private_ip_google_access = true
+
+  secondary_ip_range {
+    range_name = "live-cluster-pods"
+    ip_cidr_range = "10.20.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name = "live-cluster-services"
+    ip_cidr_range = "10.30.0.0/16"
+  }
+}
+
+resource "google_compute_firewall" "live-network-firewall" {
+  project = "${google_project.live.id}"
+  network = "${google_compute_network.live-network.name}"
+
+  name = "chmsqrt2-truesparrow-live-network-firewall"
+  description = "The firewall rules for the common network"
+
+  priority = 1000
+  direction = "INGRESS"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = [ 22, 80, 443 ] # SSH
+  }
+}
+
+resource "google_sql_database_instance" "live-sqldb-main" {
+  project = "${google_project.live.id}"
+
+  name = "chmsqrt2-truesparrow-live-sqldb-main"
+  database_version = "POSTGRES_9_6"
+  region = "${var.live_region}"
+
+  settings {
+    tier = "db-f1-micro"
+    activation_policy = "ALWAYS"
+    availability_type = "ZONAL"
+    disk_autoresize = true
+    disk_size = 10
+    disk_type = "PD_SSD"
+
+    backup_configuration {
+      enabled = false
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+      require_ssl = false # TODO: Or perhaps it should be true
+    }
+
+    location_preference {
+      zone = "europe-west1-b"
+    }
+
+    maintenance_window {
+      day = 7
+      hour = 6
+      update_track = "stable"
+    }
+  }
+
+  depends_on = [ "google_project_services.live-services" ]
+}
+
+resource "google_sql_user" "live-sqldb-user-identity" {
+  project = "${google_project.live.id}"
+  instance = "${google_sql_database_instance.live-sqldb-main.name}"
+
+  name = "service-identity"
+  password = "${var.live_sqldb_main_user_identity_password}"
+}
+
+resource "google_sql_user" "live-sqldb-user-content" {
+  project = "${google_project.live.id}"
+  instance = "${google_sql_database_instance.live-sqldb-main.name}"
+
+  name = "service-content"
+  password = "${var.live_sqldb_main_user_content_password}"
+}
+
+resource "google_sql_database" "live-sqldb-database" {
+  project = "${google_project.live.id}"
+  instance = "${google_sql_database_instance.live-sqldb-main.name}"
+
+  name = "truesparrow"
+  charset = "UTF8"
+  collation = "en_US.UTF8"
+
+  depends_on = [
+    "google_sql_user.live-sqldb-user-identity",
+    "google_sql_user.live-sqldb-user-content"
+  ]
+}
+
+resource "google_container_cluster" "live-cluster" {
+  project = "${google_project.live.id}"
+  zone = "${var.live_region_and_zone}"
+
+  name = "chmsqrt2-truesparrow-live-cluster"
+  description = "The common cluster for all the services"
+  min_master_version = "1.9.4-gke.1"
+  node_version = "1.9.4-gke.1"
+
+  network = "${google_compute_network.live-network.self_link}"
+  subnetwork = "${google_compute_subnetwork.live-subnetwork.name}"
+  ip_allocation_policy {
+    cluster_secondary_range_name = "live-cluster-pods"
+    services_secondary_range_name = "live-cluster-services"
+  }
+
+  logging_service = "logging.googleapis.com"
+  monitoring_service = "monitoring.googleapis.com"
+
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = "03:00"
+    }
+  }
+
+  master_auth {
+    username = "truesparrow"
+    password = "${var.live_compute_web_cluster_user_password}"
+  }
+
+  node_pool {
+    name = "chmsqrt2-truesparrow-live-pool"
+
+    initial_node_count = 1
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 1
+    }
+
+    management {
+      auto_repair = false
+      auto_upgrade = true
+    }
+
+    node_config {
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+
+      disk_size_gb = 10
+      local_ssd_count = 0
+      machine_type = "n1-highcpu-2"
+      preemptible = false
+
+      service_account = "${google_service_account.live-compute-web.email}"
+    }
+  }
+}
+
+resource "google_project_iam_binding" "live-container-cluster-admins" {
+  project = "${google_project.live.id}"
+  role = "roles/container.clusterAdmin"
+  # TODO: perhaps dont hardcode this in the future
+  members = [
+      "serviceAccount:ci-builder@chmsqrt2-truesparrow-common.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "live-container-developer" {
+  project = "${google_project.live.id}"
+  role = "roles/container.developer"
+  # TODO: perhaps dont hardcode this in the future
+  members = [
+      "serviceAccount:ci-builder@chmsqrt2-truesparrow-common.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_compute_global_address" "live-loadbalancer-address" {
+  project = "${google_project.live.id}"
+
+  name = "chmsqrt2-truesparrow-live-loadbalancer-address"
+  ip_version = "IPV4"
+}
+
+resource "google_dns_record_set" "live-adminfe-domain" {
+  project = "${data.google_project.common.id}"
+  managed_zone = "${data.google_dns_managed_zone.chmsqrt2-domain.name}"
+
+  name = "adminfe.live.truesparrow.${data.google_dns_managed_zone.chmsqrt2-domain.dns_name}"
+  type = "A"
+  ttl = "300"
+  rrdatas = [ "${google_compute_global_address.live-loadbalancer-address.address}" ]
+}
+
+resource "google_dns_record_set" "live-sitefe-domain" {
+  project = "${data.google_project.common.id}"
+  managed_zone = "${data.google_dns_managed_zone.chmsqrt2-domain.name}"
+
+  name = "sitefe.live.truesparrow.${data.google_dns_managed_zone.chmsqrt2-domain.dns_name}"
+  type = "A"
+  ttl = "300"
+  rrdatas = [ "${google_compute_global_address.live-loadbalancer-address.address}" ]
+}
+
+resource "google_dns_record_set" "live-sitefe-wildcard-domain" {
+  project = "${data.google_project.common.id}"
+  managed_zone = "${data.google_dns_managed_zone.chmsqrt2-domain.name}"
+
+  name = "*.sitefe.live.truesparrow.${data.google_dns_managed_zone.chmsqrt2-domain.dns_name}"
+  type = "A"
+  ttl = "300"
+  rrdatas = [ "${google_compute_global_address.live-loadbalancer-address.address}" ]
+}
+
+resource "google_dns_record_set" "live-adminfe-letsencrypt-challange" {
+  project = "${data.google_project.common.id}"
+  managed_zone = "${data.google_dns_managed_zone.chmsqrt2-domain.name}"
+
+  name = "_acme-challenge.adminfe.live.truesparrow.${data.google_dns_managed_zone.chmsqrt2-domain.dns_name}"
+  type = "TXT"
+  ttl = "3600"
+  rrdatas = [ "a1n5YdxIOTv_-kDPWCx01VD2IqRcpeXFAN9DRS471sk" ]
+}
+
+resource "google_dns_record_set" "live-sitefe-letsencrypt-challange" {
+  project = "${data.google_project.common.id}"
+  managed_zone = "${data.google_dns_managed_zone.chmsqrt2-domain.name}"
+
+  name = "_acme-challenge.sitefe.live.truesparrow.${data.google_dns_managed_zone.chmsqrt2-domain.dns_name}"
+  type = "TXT"
+  ttl = "3600"
+  rrdatas = [ "O5ZQB5H39h_Agr3DSF6153lMeusJmdkeadmZ5TeZIiw", "clbaHu_t3uU6olGwGZ5EBWh5zoGJrMInE6QbgLUydu4" ]
+}
+
+resource "google_compute_ssl_certificate" "live-loadbalancer-certificate" {
+  project = "${google_project.live.id}"
+
+  name = "chmsqrt2-truesparrow-live-loadbalancer-new-certificate"
+  description = "Certificate for the live global loadbalancer"
+
+  private_key = "${file("../certs-live/privkey.pem")}"
+  certificate = "${file("../certs-live/fullchain.pem")}"
 }
